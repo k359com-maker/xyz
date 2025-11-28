153c31164533c4ac82910adce70b8b1ea47dc34f
Solarax <k359.com@gmail.com>
2025-11-29 00:29:26 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -3,9 +3,10 @@ const DISCORD_USER_ID = '742692894292443187';
 const LANYARD_API_URL = `https://api.lanyard.rest/v1/users/${DISCORD_USER_ID}`;
 
 // Typing Effect Data
-const texts = ["เบื่อ", "กดเข้ามาทำไม?", "CU"];
+const texts = ["asdwasdwdw", "กดเข้ามาทำไม?", "wdasdwdaw"];
 
 let isMuted = true;
 let textIndex = 0, charIndex = 0, isDeleting = false;
+
 
 
