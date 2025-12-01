4294ea9a672958aaa0f3aa24aee8be95843c22eb
Solarax <k359.com@gmail.com>
2025-12-01 17:14:31 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,1 @@
-// Discord Lanyard API Setup
-const DISCORD_USER_ID = '742692894292443187'; // <--- UID DISCORD
-const LANYARD_API_URL = `https://api.lanyard.rest/v1/users/${DISCORD_USER_ID}`;
-
-// Typing Effect Data
-const texts = ["Developer", "REN"];
-
-let isMuted = true;
-let textIndex = 0, charIndex = 0, isDeleting = false;
+const DISCORD_USER_ID='742692894292443187';const LANYARD_API_URL=`https://api.lanyard.rest/v1/users/${DISCORD_USER_ID}`;const texts=["Developer","REN"];let isMuted=!0;let textIndex=0,charIndex=0,isDeleting=!1
